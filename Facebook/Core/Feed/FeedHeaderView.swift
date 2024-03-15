a83462000f9dbe3404a06cf44397847364f63005
abdulmajit <abkubatt@gmail.com>
2024-03-16 00:51:09 +0600

    layouted top side of FeedView

---
+++ Facebook/Core/Feed/FeedHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedHeaderView.swift
+//  Facebook
+//
+//  Created by Abdulmajit Kubatbekov on 16/3/24.
+//
+
+import SwiftUI
+
+struct FeedHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedHeaderView()
+}
---
+++ Facebook/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Facebook
+//
+//  Created by Abdulmajit Kubatbekov on 16/3/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
