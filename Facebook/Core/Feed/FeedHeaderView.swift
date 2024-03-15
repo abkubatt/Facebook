43bc834cc813738c16012a24cd961bffd9f0a875
abdulmajit <abkubatt@gmail.com>
2024-03-16 00:51:14 +0600

    layouted top side of FeedView

--- Facebook/Core/Feed/FeedHeaderView.swift
+++ Facebook/Core/Feed/FeedHeaderView.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct FeedHeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            NavigationLink {
+                ProfileView()
+            } label: {
+                Image("profilePic")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+            }
+            
+            Button {
+                
+            } label: {
+                HStack {
+                    Text("What's on your main?")
+                        .foregroundStyle(.black)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .padding(.vertical, 10)
+                .overlay {
+                    Capsule()
+                        .stroke(Color(.systemGray4), lineWidth: 1)
+                }
+                .padding(.leading, 5)
+                .padding(.trailing, 15)
+            }
+            
+            Image(systemName: "photo.on.rectangle.angled")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 20, height: 20)
+                .foregroundStyle(.green)
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 30)
     }
 }
 
--- Facebook/Core/Feed/FeedView.swift
+++ Facebook/Core/Feed/FeedView.swift
@@ -9,7 +9,39 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                FeedHeaderView()
+                Spacer()
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Text("Facebook")
+                        .font(.system(size: 32, weight: .bold))
+                        .foregroundStyle(.blue)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 20) {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 20, height: 20)
+                        
+                        Image(systemName: "magnifyingglass")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 20, height: 20)
+                            .font(.system(size: 18, weight: .bold))
+                        
+                        Image("messenger")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 20, height: 20)
+                    }
+                }
+            }
+        }
     }
 }
 
