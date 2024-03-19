702d67e62da11ddb51cb751e7a8886e35194d97d
abdulmajit <abkubatt@gmail.com>
2024-03-19 22:15:16 +0600

    completed layouting of StoriesView

---
+++ Facebook/Core/Feed/MyStoryCardView.swift
@@ -0,0 +1,18 @@
+//
+//  MyStoryCardView.swift
+//  Facebook
+//
+//  Created by Abdulmajit Kubatbekov on 19/3/24.
+//
+
+import SwiftUI
+
+struct MyStoryCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyStoryCardView()
+}
---
+++ Facebook/Core/Feed/StoryCardView.swift
@@ -0,0 +1,18 @@
+//
+//  StoryCardView.swift
+//  Facebook
+//
+//  Created by Abdulmajit Kubatbekov on 19/3/24.
+//
+
+import SwiftUI
+
+struct StoryCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryCardView()
+}
