477abf0b848d6629aea7ae61dced6e668439d001
abdulmajit <abkubatt@gmail.com>
2024-03-21 23:25:29 +0600

    complete layout post view

---
+++ Facebook/Core/Components/Views/PostView.swift
@@ -0,0 +1,18 @@
+//
+//  PostView.swift
+//  Facebook
+//
+//  Created by Abdulmajit Kubatbekov on 21/3/24.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostView()
+}
