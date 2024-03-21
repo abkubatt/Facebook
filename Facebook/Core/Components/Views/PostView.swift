5a51093c4f0f9f0a17d91b0df3341e534406e4af
abdulmajit <abkubatt@gmail.com>
2024-03-21 23:25:35 +0600

    complete layout post view

--- Facebook/Core/Components/Views/PostView.swift
+++ Facebook/Core/Components/Views/PostView.swift
@@ -9,7 +9,91 @@ import SwiftUI
 
 struct PostView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+                Image("profilePic")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Abdulmajit Kubatbekov")
+                        .font(.system(size: 14, weight: .semibold))
+                    
+                    HStack(spacing: 5) {
+                        Text("1 d")
+                        Circle()
+                            .frame(width: 2, height: 2)
+                            .fontWeight(.bold)
+                        Image(systemName: "globe")
+                    }
+                    .font(.system(size: 12))
+                    .foregroundStyle(.blue)
+                }
+                Spacer()
+                HStack(spacing: 24) {
+                    Image(systemName: "ellipsis")
+                    Image(systemName: "xmark")
+                }
+                .foregroundStyle(Color(.darkGray))
+                .fontWeight(.bold)
+            }
+            .padding(.horizontal)
+            Text("Time to party with team")
+                .padding(.horizontal)
+            Image("office")
+                .resizable()
+                .scaledToFill()
+            
+            HStack(spacing: 3) {
+                Image("like")
+                    .resizable()
+                    .frame(width: 18, height: 18)
+                
+                Text("5")
+                Spacer()
+                Text("3 comments")
+                Text("•")
+                    .fontWeight(.bold)
+                Text("2 shares")
+            }
+            
+            .padding(.horizontal)
+            .font(.system(size: 12))
+            .foregroundStyle(.blue)
+            
+            Divider()
+                .background(.white.opacity(0.5))
+            
+            HStack {
+                Spacer()
+                
+                HStack {
+                    Image(systemName: "hand.thumbsup")
+                    Text("Like")
+                }
+                Spacer()
+                HStack {
+                    Image(systemName: "message")
+                    Text("Comment")
+                }
+                Spacer()
+                HStack {
+                    Image("icone-messager-noir")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    Text("Send")
+                }
+                Spacer()
+                HStack {
+                    Image(systemName: "arrowshape.turn.up.right")
+                    Text("Share")
+                }
+                Spacer()
+            }
+            .font(.system(size: 12))
+            .foregroundStyle(.blue)
+        }
     }
 }
 
--- Facebook/Core/Feed/FeedView.swift
+++ Facebook/Core/Feed/FeedView.swift
@@ -11,39 +11,45 @@ struct FeedView: View {
     var body: some View {
         NavigationStack {
             GeometryReader { proxy in
-                VStack {
-                    FeedHeaderView()
-                    DividerView(width: proxy.size.width)
-                    StoryFeedView()
-                    DividerView(width: proxy.size.width)
-                    Spacer()
-                }
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        Text("Facebook")
-                            .font(.system(size: 32, weight: .bold))
-                            .foregroundStyle(.blue)
-                    }
-                    
-                    ToolbarItem(placement: .topBarTrailing) {
-                        HStack(spacing: 20) {
-                            Image(systemName: "plus.circle.fill")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 20, height: 20)
-                            
-                            Image(systemName: "magnifyingglass")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 20, height: 20)
-                                .font(.system(size: 18, weight: .bold))
-                            
-                            Image("messenger")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 20, height: 20)
+                ScrollView {
+                    VStack {
+                        FeedHeaderView()
+                        DividerView(width: proxy.size.width)
+                        StoryFeedView()
+                        DividerView(width: proxy.size.width)
+                        ForEach(0 ..< 10) { _ in
+                            PostView()
                         }
+                        DividerView(width: proxy.size.width - 15)
+                        Spacer()
                     }
+                    .toolbar {
+                        ToolbarItem(placement: .topBarLeading) {
+                            Text("Facebook")
+                                .font(.system(size: 32, weight: .bold))
+                                .foregroundStyle(.blue)
+                        }
+                        
+                        ToolbarItem(placement: .topBarTrailing) {
+                            HStack(spacing: 20) {
+                                Image(systemName: "plus.circle.fill")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 20, height: 20)
+                                
+                                Image(systemName: "magnifyingglass")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 20, height: 20)
+                                    .font(.system(size: 18, weight: .bold))
+                                
+                                Image("messenger")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 20, height: 20)
+                            }
+                        }
+                }
                 }
             }
         }
