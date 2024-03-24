531f17cba9a98d73e85cf3aeaebc7adc7969b3ef
abdulmajit <abkubatt@gmail.com>
2024-03-25 02:41:48 +0600

    finished Friend Request View

---
+++ Facebook/Core/Friends/View/FriendsView.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsView.swift
+//  Facebook
+//
+//  Created by Abdulmajit Kubatbekov on 25/3/24.
+//
+
+import SwiftUI
+
+struct FriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendsView()
+}
