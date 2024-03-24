a6e69c1ef99c91c629d922d7d3ff8b164726a987
abdulmajit <abkubatt@gmail.com>
2024-03-24 22:41:03 +0600

    completed friends part of profileview

--- Facebook/Core/Profile/ProfileFriendsView.swift
+++ Facebook/Core/Profile/ProfileFriendsView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct ProfileFriendsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    private var gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    private var width: CGFloat
 
-#Preview {
-    ProfileFriendsView()
+    init(width: CGFloat) {
+        self.width = width
+    }
+
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Text("Friends")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                    
+                    Text("4 friends")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                }
+                
+                Spacer()
+                
+                Text("Find Friends")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.blue)
+            }
+            LazyVGrid(columns: gridItems) {
+                ForEach(0 ..< 10) { _ in
+                    VStack {
+                        Image("profilePic1")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: width / 3 - 20, height: width / 3 - 20)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                        Text("Abdulmajit Kubatbekov")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                    }
+                }
+            }
+            Text("See all friends")
+                .font(.headline)
+                .fontWeight(.semibold)
+                .frame(width: width - 40, height: 30)
+                .background(Color(.systemGray5))
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+                .padding(.vertical)
+                .foregroundStyle(Color(.darkGray))
+        }
+        .padding(.horizontal)
+    }
 }
--- Facebook/Core/Profile/ProfileView.swift
+++ Facebook/Core/Profile/ProfileView.swift
@@ -16,6 +16,9 @@ struct ProfileView: View {
                 VStack {
                     ProfileHeaderView(width: proxy.size.width)
                     DividerView(width: proxy.size.width)
+                    ProfileOptionsView()
+                    ProfileFriendsView(width: proxy.size.width)
+                    DividerView(width: proxy.size.width)
                 }
             }
             .navigationBarTitleDisplayMode(.inline)
