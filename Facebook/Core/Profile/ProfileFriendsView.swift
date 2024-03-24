d2d8a1ae951a941f5e67d881b1e6e9bc68b4f184
abdulmajit <abkubatt@gmail.com>
2024-03-24 22:40:57 +0600

    completed friends part of profileview

---
+++ Facebook/Core/Profile/ProfileFriendsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileFriendsView.swift
+//  Facebook
+//
+//  Created by Abdulmajit Kubatbekov on 24/3/24.
+//
+
+import SwiftUI
+
+struct ProfileFriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileFriendsView()
+}
---
+++ Facebook/Core/Profile/ProfileOptionsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileOptionsView.swift
+//  Facebook
+//
+//  Created by Abdulmajit Kubatbekov on 24/3/24.
+//
+
+import SwiftUI
+
+struct ProfileOptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileOptionsView()
+}
