3ba84879c02fd726afa622d96360d1a11ea7a037
abdulmajit <abkubatt@gmail.com>
2024-03-25 00:41:07 +0600

    finished bottom view of profile

---
+++ Facebook/Core/Profile/ReelResuableView.swift
@@ -0,0 +1,18 @@
+//
+//  ReelResuableView.swift
+//  Facebook
+//
+//  Created by Abdulmajit Kubatbekov on 25/3/24.
+//
+
+import SwiftUI
+
+struct ReelResuableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReelResuableView()
+}
