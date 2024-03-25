de33f9d825f896a815d496790be83e7eedc1c309
abdulmajit <abkubatt@gmail.com>
2024-03-25 14:43:18 +0600

    Finished header part of menu

---
+++ Facebook/Core/Menu/View/ShortCutsView.swift
@@ -0,0 +1,18 @@
+//
+//  ShortCutsView.swift
+//  Facebook
+//
+//  Created by Abdulmajit Kubatbekov on 25/3/24.
+//
+
+import SwiftUI
+
+struct ShortCutsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShortCutsView()
+}
