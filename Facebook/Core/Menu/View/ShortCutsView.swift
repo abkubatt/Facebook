1522a81930179670b805ddf860b78ea15b320620
abdulmajit <abkubatt@gmail.com>
2024-03-25 14:43:22 +0600

    Finished header part of menu

--- Facebook/Core/Menu/View/ShortCutsView.swift
+++ Facebook/Core/Menu/View/ShortCutsView.swift
@@ -8,11 +8,63 @@
 import SwiftUI
 
 struct ShortCutsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
 
-#Preview {
-    ShortCutsView()
+    private var width: CGFloat
+
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 15),
+        .init(.flexible(), spacing: 15)
+    ]
+    
+    private let shortcustsArray: [(String, String)] = [
+        ("Memories", "gobackward"),
+        ("Saved", "bookmark.fill"),
+        ("Groups", "person.2.circle"),
+        ("Video", "play.tv.fill"),
+        ("Marketplace", "storefront.fill"),
+        ("Friends", "person.2.fill"),
+        ("Feeds", "calendar.badge.clock"),
+        ("Events", "calendar")
+    ]
+    
+    init(width: CGFloat) {
+        self.width = width
+    }
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Your shortcuts")
+                .font(.headline)
+                .foregroundStyle(Color(.darkGray))
+            LazyVGrid(columns: gridItems, spacing: 15) {
+                ForEach(0..<8) { index in
+                    VStack(alignment: .leading, spacing: 12) {
+                        Image(systemName: shortcustsArray[index].1)
+                            .foregroundStyle(.blue)
+                        Text(shortcustsArray[index].0)
+                            .font(.headline)
+                        HStack { Spacer() }
+                    }
+                    .padding(.horizontal)
+                    .frame(width: width * 0.45, height: 80)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+            }
+            
+            Button {
+                
+            } label: {
+                Text("See more")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .frame(width: width - 30, height: 44)
+                    .background(Color(.systemGray5))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .foregroundStyle(.black)
+                    .padding(.vertical)
+            }
+        }
+        .padding(.horizontal)
+    }
 }
